9386d65defb6193585538aa8f0da5ffd928892d0
Dave <dave@Daves-MacBook-Pro.local>
2017-09-21 18:14:28 +0100

    re-write server

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Product = require('./api/models/productModel'),
+  Supplier = require('./api/models/supplierModel'),
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/db'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/api'); 
+routes(app); 
+
 
 app.listen(port);
